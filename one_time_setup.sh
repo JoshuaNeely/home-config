3666e54eebf1051ee6b271197fd2a846d7f9200b
JoshuaNeely <joshua.neely@adtran.com>
2018-03-09 09:33:19 -0600

    Add necessary git clone, for installing Vundle.

--- one_time_setup.sh
+++ one_time_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# pull down Vundle; plugin manager for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # automagically install Vundle plugins from .vimrc
 vim +PluginInstall +qall
