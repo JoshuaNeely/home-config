c37392e9de6d09809b2e4013ddaa236434a0fc47
JoshuaNeely <joshua.a.neely@gmail.com>
2021-12-18 15:09:32 -0600

    fix: bad magit highlighting

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nord)
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -112,6 +112,9 @@
 
 ;; org publishing
 (load "~/.doom.d/config-publishing.el")
+
+;; ctags magic
+(load "~/.doom.d/config-ctags.el")
 
 
 ;; customize default emacs font
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,18 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
  '(package-selected-packages
-   (quote
-    (copy-as-format ox-jira edit-server org-jira org-roam-server simple-httpd org-journal)))
+   '(copy-as-format ox-jira edit-server org-jira org-roam-server simple-httpd org-journal))
  '(safe-local-variable-values
-   (quote
-    ((eval org-update-all-dblocks)
+   '((eval org-update-all-dblocks)
      (org-confirm-babel-evaluate)
-     (eval org-clock-in))))
- '(send-mail-function (quote sendmail-send-it)))
+     (eval org-clock-in)))
+ '(send-mail-function 'sendmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-diff-added ((t (:extend t :background "black" :foreground "#7a9551"))))
+ '(magit-diff-added-highlight ((t (:extend t :background "black" :foreground "#99bb66" :weight bold))))
+ '(magit-diff-removed-highlight ((t (:extend t :background "black" :foreground "#ff6655" :weight bold)))))
