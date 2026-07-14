59290ddb29dfed430590a089e8b2371b2fc61512
JoshuaNeely <joshua.a.neely@gmail.com>
2026-07-14 10:32:56 -0500

    update nvim auto complete to appear 500ms later

---
+++ stow/dot-config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          -- Delays the completion menu when typing
+          auto_show_delay_ms = 500,
+        },
+      },
+      signature = {
+        -- Delays the function signature help pop-up
+        auto_show_delay_ms = 500,
+      },
+    },
+  },
+}
